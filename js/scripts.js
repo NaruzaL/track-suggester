a81e8ddc9a41f8ec93a1ed6a04dd0ea584cf3db1
PETE LAZURAN <pdlazuran@gmail.com>
2017-04-28 12:06:47 -0700

    fixes if else's to make all results achievable.

--- js/scripts.js
+++ js/scripts.js
@@ -9,21 +9,21 @@ $(document).ready(function(){
 
     //backend functions begin
 
-    if (pc ==="0" && company === "0" && art === "0" || art === "1" && fb == "0" || fb === "1" && droid === "1"){
+    if (pc ==="0" && company === "0" && art === "1" && fb == "0" || fb === "1" && droid === "1"){
       $("#java").hide();
       $("#ruby").hide();
       $("#php").hide();
       $("#css").hide();
       $("#c").show();
     }
-    else if (pc === "1" && droid === "1"){
+    else if (pc === "1" && company === "1"){
       $("#java").hide();
       $("#php").hide();
       $("#c").hide();
       $("#css").hide();
       $("#ruby").show();
     }
-    else if (art === "0" ){
+    else if (art === "0" && company === "0" || company === "1" ){
       $("#java").hide();
       $("#ruby").hide();
       $("#php").hide();
